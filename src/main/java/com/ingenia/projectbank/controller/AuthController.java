b506121e47e5a35c43d0ae23a3e11d43a1f8ae44
salvi <salvilopezronda@gmail.com>
2021-05-12 00:33:34 +0200

    add logger in AuthController

--- src/main/java/com/ingenia/projectbank/controller/AuthController.java
+++ src/main/java/com/ingenia/projectbank/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.ingenia.projectbank.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
+    private final Logger log = LoggerFactory.getLogger(AuthController.class);
 }
