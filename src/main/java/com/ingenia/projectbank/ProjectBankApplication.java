ae50e699304d827a41bc1e715043492c6bb98a7e
salvador <salvilopezronda@gmail.com>
2021-05-11 12:39:42 +0200

    git init

---
+++ src/main/java/com/ingenia/projectbank/ProjectBankApplication.java
@@ -0,0 +1,13 @@
+package com.ingenia.projectbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingenia/projectbank/ProjectBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenia.projectbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
